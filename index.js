b484f4734cd4b1ca352a1bd22a8b0b58382d44f7
BrianOnchwari <onchwaribrian6@gmail.com>
2024-03-24 14:03:09 +0300

    Arrow Functions

---
+++ index.js
@@ -0,0 +1,17 @@
+// Function expression for division
+const divide = function(a, b) {
+    return a / b;
+}
+
+// Arrow function for squaring
+const square = x => x * x;
+
+// Arrow function for addition
+const add = (a, b) => a + b;
+
+// Exporting functions
+module.exports = {
+    divide,
+    square,
+    add
+};
